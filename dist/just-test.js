41f8d44cbf5a2eb7436f3d153b6e4ae49297be83
金伟强 <iamjoel007@gmail.com>
2016-08-17 18:05:12 +0800

    adds

--- dist/just-test.js
+++ dist/just-test.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "dist";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
@@ -45,8 +45,84 @@
 /*!**********************************!*\
   !*** ./demos/just-test/index.js ***!
   \**********************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	console.log(__webpack_require__(/*! ./style.css */ 1) + '');
+
+/***/ },
+/* 1 */
+/*!***********************************!*\
+  !*** ./demos/just-test/style.css ***!
+  \***********************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(/*! ./../../~/css-loader/lib/css-base.js */ 2)();
+	// imports
+	
+	
+	// module
+	exports.push([module.id, "body{\r\n  margin: 0;\r\n}", ""]);
+	
+	// exports
+
+
+/***/ },
+/* 2 */
+/*!**************************************!*\
+  !*** ./~/css-loader/lib/css-base.js ***!
+  \**************************************/
 /***/ function(module, exports) {
 
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+	
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+	
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
 
 
 /***/ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 var path = require('path');
 var srcPrefix = './demos/';
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var webpackConfig = {
   // devtool: 'eval-source-map',
-  entry: {
-  },
+  entry: {},
   output: {
     'path': 'dist',
     filename: '[name].js',
     trunkFilename: '[name].bundle.js',
-    // publicPath: './assets/js/' // 用异步加载模块一定要加这个
+    publicPath: 'dist' // 用异步加载模块一定要加这个
   },
   module: {
     loaders: [
@@ -18,7 +18,11 @@ var webpackConfig = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'file?hash=sha512&digest=hex&name=[hash].[ext]'
       },
-      { test: /\.css$/, loader: "css-loader" }
+      // { test: /\.css$/, loader: ExtractTextPlugin.extract({
+      //           fallbackLoader: "style-loader",
+      //           loader: "css-loader"
+      //       }) }
+      { test: /\.css$/, loader: 'css-loader' }
     ]
   },
   plugins: [],
@@ -30,26 +34,45 @@ var webpackConfig = {
 /*
 读 demos 下第一层目录
 */
-var init = (webpackConfig) =>{
-  // 
+var init = (webpackConfig) => {
   var eachDemoConfig = addConfig();
   Object.assign(webpackConfig.entry, eachDemoConfig.entry);
+  // webpackConfig.plugins.push(eachDemoConfig.css);
   webpackConfig.plugins.push(eachDemoConfig.html);
+  // console.log(webpackConfig.plugins);
 };
 
-var addConfig = (demoFolderName) =>{
+var addConfig = (demoFolderName) => {
+  var name = 'just-test';
+  var entry = {};
+  entry[name] = `${srcPrefix}${name}/index.js`;
   return {
-    entry: {
-      'just-test': srcPrefix + 'just-test/index',
-    },
+    entry: entry,
+    // css: new ExtractTextPlugin(srcPrefix + "just-test/style.css"),
     html: new HtmlWebpackPlugin({
-      filename: 'test.html',
-      title: 'just test',
+      filename: `${name}.html`,
+      title: `${name}`,
+      inject: false,
       // For details on `!!` see https://webpack.github.io/docs/loaders.html#loader-order
       template: '!!ejs!templates/normal.html',
-      js: ['dist/just-test/index.js'],
-      css: ['.css-loader!demo/just-test/style.css'],// TODO
-      content: '<div>aaaa</div>'//TODO
+      js: [`${name}.js`],
+      // extraFiles: `${srcPrefix}${name}just-test/style.css`,
+      css: ['./style.css'], // TODO
+      content: '<div>aaaa</div>', //TODO
+      "files": {
+        "css": [srcPrefix + "just-test/style.css"],
+        "js": [srcPrefix + "just-test/index.js"],
+        "chunks": {
+          "head": {
+            "entry": srcPrefix + "just-test/index.js",
+            "css": [srcPrefix + "just-test/style.css"]
+          },
+          // "main": {
+          //   "entry": "assets/main_bundle.js",
+          //   "css": []
+          // },
+        }
+      }
 
     })
   }
