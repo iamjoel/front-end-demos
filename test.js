c961d888b0326abb2cfc00cf1ac9823654f7cb36
金伟强 <iamjoel007@gmail.com>
2016-08-18 18:00:49 +0800

    adds

--- test.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-// console.log(fs.readdirSync('demos'));
-fs.readdirSync('demos').forEach(function (foldName) {
-  console.log(fs.statSync(`demos/${foldName}/style.css`))
-  console.log(fs.statSync(`demos/${foldName}/style1.css`))
-});
