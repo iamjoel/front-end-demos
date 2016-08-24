c8c6dcc62678a8a50d6cb2f0656727cce502a307
金伟强 <iamjoel007@gmail.com>
2016-08-24 23:17:21 +0800

    adds

--- demos/html-struct/index.js
+++ demos/html-struct/index.js
@@ -4,5 +4,24 @@ new Vue({
   data: {
     isInMobile: false,
     isUseLatest: false
+  },
+  computed: {
+    html: function () {
+      let mobileMeta = this.isInMobile ? '<meta name="viewport" content="width=device-width, initial-scale=1">' : '';
+      let useLatest = this.isUseLatest ? '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' : '';
+      return `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                  <meta charset="UTF-8">
+                  <title>Document</title>
+                  ${mobileMeta}
+                  ${useLatest}
+                  </head>
+                <body>
+                  <!--在页面中显示的主体内容 -->
+                  <div> sth </div>
+                </body>
+              </html>`
+    }
   }
 })
