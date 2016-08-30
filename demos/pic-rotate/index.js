6b3ace73aedd7740ca039399e0aa2ce368a30fb0
金伟强 <iamjoel007@gmail.com>
2016-08-30 23:03:33 +0800

    add pic rotota

--- demos/pic-rotate/index.js
+++ demos/pic-rotate/index.js
@@ -8,30 +8,47 @@ new Vue({
       width: $imgWrap.width(),
       height: $imgWrap.height()
     }
-    let wrapWidthBiger = wrapSize.width > wrapSize.height
-    getImgSize($('.target').attr('src')).then((imgSize) => {
-      // this.imgSize = imgSize
-      // var imgWidthBiger = imgSize.width > imgSize.height
-      // if (wrapWidthBiger === imgWidthBiger) {
-      //   $('.target').width('100%')
-      // } else {
-      // }
-    })
+    getImgSize($('.target').attr('src')).then(function (imgSize) {
+      let imgRatio = imgSize.width / imgSize.height
+      let imgW = wrapSize.width
+      let imgH = imgW / imgRatio
+      if (imgH < wrapSize.height) {
+        this.imgSizeInfo.push([wrapSize.width + 'px', 'auto'])
+      } else {
+        this.imgSizeInfo.push(['auto', wrapSize.height + 'px'])
+      }
+      // 旋转90度后
+      imgW = wrapSize.height
+      imgH = imgW / imgRatio
+      if (imgH < wrapSize.width) {
+        this.imgSizeInfo.push([wrapSize.height + 'px', 'auto'])
+      } else {
+        this.imgSizeInfo.push(['auto', wrapSize.width + 'px'])
+      }
+
+      this.styleObject.width = this.imgSizeInfo[0][0]
+      this.styleObject.height = this.imgSizeInfo[0][1]
+    }.bind(this))
   },
   data: {
+    imgSizeInfo: [],
+    styleObject: {
+      rotateDegree: 0,
+      width: 0,
+      height: 0
+    },
     rotateDegree: 0
   },
   methods: {
     rotate: function (degree) {
       this.rotateDegree += degree
-    }
-  },
-  computed: {
-    rotateStyle: function () {
-      // var scale = this.rotateDegree % 180 === 0 ? 1 : .73
-      var scale = 1
-      return {
-        transform: `rotate(${this.rotateDegree}deg) scale(${scale})`
+      this.styleObject.transform = `rotate(${this.rotateDegree}deg)`
+      if (this.rotateDegree % 180 === 0) {
+        this.styleObject.width = this.imgSizeInfo[0][0]
+        this.styleObject.height = this.imgSizeInfo[0][1]
+      } else {
+        this.styleObject.width = this.imgSizeInfo[1][0]
+        this.styleObject.height = this.imgSizeInfo[1][1]
       }
     }
   }
